9b7ff2b8ce891335ace7d1dbd018e8424f973245
Flavia Voicu <flavia.voicuu@gmail.com>
2024-09-30 15:38:40 +0200

    Add decalrations

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,46 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+
+class Bureaucrat {
+ private:
+	const std::string _name;
+	int								_grade;
+
+ public:
+	/**Orthodox Canonical Form Elements*/
+	Bureaucrat() = default;
+	Bureaucrat(const std::string& name, int grade);
+	Bureaucrat(const Bureaucrat& other) = default;
+	Bureaucrat& operator=(const Bureaucrat& other) = default;
+	~Bureaucrat() = default;
+
+	/**Getters**/
+	const std::string& getName() const;
+	int getGrade() const;
+
+	/**Grade Manipulation**/
+	void incrementGrade();
+	void decrementGrade();
+
+	/**Custom exception classes**/
+  class GradeTooHighException : public std::exception {
+    public:
+        const char* what() const noexcept override;
+  };
+
+  class GradeTooLowException : public std::exception {
+    public:
+        const char* what() const noexcept override;
+  };
+
+};
+
+/**Insertion operator overload**/
+std::ostream& operator<<(std::ostream &out, const Bureaucrat &b);
+
+#endif
