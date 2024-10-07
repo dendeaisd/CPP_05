38419a7b6151a4b578a6a1d77cb4c12254a891e6
Flavia Voicu <flavia.voicuu@gmail.com>
2024-10-07 09:24:48 +0200

    OCF completed

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -1,9 +1,11 @@
 #include "Form.hpp"
 
+/*Default constuctor*/
 Form::Form()
     : _name("Empty Form"), _isSigned(false), _gradeToSign(150),
       _gradeToExecute(150) {};
 
+/*Parametrized constructor*/
 Form::Form(const std::string &name, int gradeToSign, int gradeToExecute)
     : _name(name), _isSigned(false), _gradeToSign(gradeToSign),
       _gradeToExecute(gradeToExecute) {
@@ -13,3 +15,19 @@ Form::Form(const std::string &name, int gradeToSign, int gradeToExecute)
     throw Form::GradeTooLowException();
   }
 }
+
+/*Copy constructor*/
+Form::Form(const Form &other)
+    : _name(other._name), _isSigned(other._isSigned),
+      _gradeToSign(other._gradeToSign), _gradeToExecute(other._gradeToExecute) {
+}
+
+/*Assignment operator*/
+Form &Form::operator=(const Form &other) {
+  if (this != &other)
+    _isSigned = other._isSigned;
+  return *this;
+}
+
+/*Destructor*/
+Form::~Form() {};
