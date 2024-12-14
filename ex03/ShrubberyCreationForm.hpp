710064adbf3c6037a6a511ff2be783ef159eeaf3
Flavia Voicu <flavia.voicuu@gmail.com>
2024-12-14 21:52:22 +0100

    Add previous commit corection

--- ex03/ShrubberyCreationForm.hpp
+++ ex03/ShrubberyCreationForm.hpp
@@ -14,7 +14,7 @@ public:
   ShrubberyCreationForm() = default;
   ShrubberyCreationForm(const std::string &target);
   ShrubberyCreationForm(ShrubberyCreationForm const &other) = default;
-  ShrubberyCreationForm &operator=(ShrubberyCreationForm const &other) = default;
+  ShrubberyCreationForm &operator=(ShrubberyCreationForm const &other) = delete;
   ~ShrubberyCreationForm() = default;
   
   void execute(Bureaucrat const &executor) const override;
