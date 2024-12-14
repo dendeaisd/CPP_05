51b0d41061036e5b27e843ba1138310a76a0efda
dendeaisd <flavia.voicuu@gmail.com>
2024-12-14 19:14:08 +0200

    Add  previous changes to this execise also

--- ex03/ShrubberyCreationForm.hpp
+++ ex03/ShrubberyCreationForm.hpp
@@ -11,6 +11,11 @@ private:
   std::string _target;
 
 public:
+  ShrubberyCreationForm() = default;
   ShrubberyCreationForm(const std::string &target);
+  ShrubberyCreationForm(ShrubberyCreationForm const &other) = default;
+  ShrubberyCreationForm &operator=(ShrubberyCreationForm const &other) = default;
+  ~ShrubberyCreationForm() = default;
+  
   void execute(Bureaucrat const &executor) const override;
 };
